38c47f09a5f98df6b2d59e09fdb639ed5f405547
rarebu <deluminator@protonmail.com>
2020-06-22 14:05:49 +0200

    defining first tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,16 @@
-extern crate rust-p2p;
+extern crate ipaddress;
+use std::net::Ipv4Addr;
+use ipaddress::IPAddress;
 
 #[test]
-fn first_test() {}
+fn create_node() {
+    let node = Node::new();
+    assert_eq!(node.is_running, true)
+}
 
+fn try_connection() {
+    let ip = Ipv4Addr::new(127, 0, 0, 1);
+    let node = Node::new();
+    node.connect(ip);
+    assert_eq!(node.is_connected, true)
+}
