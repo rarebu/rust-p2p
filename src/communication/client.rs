7c25722ee88343bf00057e69ba88e2e97dfbd83b
rarebu <deluminator@protonmail.com>
2020-07-10 15:34:11 +0200

    communication now working

--- src/communication/client.rs
+++ src/communication/client.rs
@@ -1,7 +1,13 @@
-use tokio::net::TcpListener;
+use tokio::net::TcpStream;
 use tokio::prelude::*;
+use tokio::net::lookup_host;
 
 #[tokio::main]
-async fn client() {
+pub async fn client() {
+    println!("client\n");
+    let mut stream = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+    println!("created stream");
 
+    let result = stream.write(b"hello world\n").await;
+    println!("wrote to stream; success={:?}", result.is_ok());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 extern crate rust_p2p;
 use std::thread::sleep;
-
-use rust_p2p::communication::server::Server;
+use futures::executor::block_on;
+use rust_p2p::communication::server::server;
+use rust_p2p::communication::client::client;
+use std::{time, thread};
 
 pub fn main() {
-    let x = Server::start();
-
-    print!("main");
-    sleep(std::time::Duration::from_secs(5));
+    let server_thread = thread::spawn(|| {
+        match server() {
+            Ok(_) => print!("ok\n"),
+            Err(_) => print!("notok\n"),
+            _ => {}
+        }
+    });
+    sleep(time::Duration::from_secs(2));
+    client();
+    println!("main\n");
 }
