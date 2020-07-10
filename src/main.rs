9e799b8457f43d7d3680c5402f0f538cc6f3a2d3
rarebu <deluminator@protonmail.com>
2020-07-10 13:09:23 +0200

    testing a server client tokio messaging system main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate rust_p2p;
+use std::thread::sleep;
 
-use rust_p2p::communication::server::server;
+use rust_p2p::communication::server::Server;
 
 pub fn main() {
-    let x = server();
-    print!("asdfasdf");
+    let x = Server::start();
+
+    print!("main");
+    sleep(std::time::Duration::from_secs(5));
 }
